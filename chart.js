8765b62528ac133880825c20f02fa91e3b26dce4
Mark Matthews <markmatthewsphd@gmail.com>
2017-03-30 09:11:25 -0500

    added comments to code

--- chart.js
+++ chart.js
@@ -1,29 +1,29 @@
 myChart.on('legendselectchanged', function(params) {
-    // printing data name in console
-    var legendObject = params.selected;
+    
+    var legendObject = params.selected; // create variable from the selected: { object }
+// console.log(legendObject) // {name: boolean-value, name: boolean-value}
+    // console.log(Object.values(legendObject)); // log to the console boolean values out of the key: value pair 
 
-    console.log(Object.values(legendObject));
+    var value1 = (Object.values(legendObject)[0]); // assign the boolean value in index[0] to value1
 
-    var value1 = (Object.values(legendObject)[0]);
-
-    var value2 = (Object.values(legendObject)[1]);
-
-    var option2 = {
+    var value2 = (Object.values(legendObject)[1]); // assign the boolean value in index[1] to value2
+// create a new option object to pass into myChart.setOption()
+    var option2 = { 
         yAxis: [{
             //show: value1,
             axisLabel: {
-                show: value1
+                show: value1  // set axisLabel[0] visability to true | false base on value1
             }
         }, {
             //show: value2,
             axisLabel: {
-                show: value2
+                show: value2 // set axisLabel[1] visability to true | false base on value2
             }
 
         }]
     };
-    myChart.setOption(option2);
+    myChart.setOption(option2);  // pass new option object into myCharts
 
-    console.log(legendObject)
-    console.log(params.name)
+    
+    // console.log(params.name)
 })
